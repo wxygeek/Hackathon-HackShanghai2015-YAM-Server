222efa748040a9a9e23672bb9cd65e73effba8a2
GKuChan <gkuchan_root@outlook.com>
2015-11-08 06:37:12 +0800

    update

--- db.js
+++ db.js
@@ -34,7 +34,7 @@ exports.storage = [{
   hasCountDown: true,
   yesBtnText: '自动订票',
   noBtnText: '手动处理',
-  description: '来自 idol [Public Info]\n自动为你订购TFBOY上海演唱会第一排VIP座位，售价18888∑元。',
+  description: '来自 idol [Public Info]\n自动为你订购TFBOY上海演唱会第一排VIP座位，售价18888元。',
   picUrl: 'http://api.itimepost.com:7777/img/tf.jpg'
 }, {
   title: '检测到小偷进入到你的房间！',
